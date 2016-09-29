fcec0c9b27cae10eee9fb3f3d19f2d4f6896a26d
malcolli <malcolli2185@gmail.com>
2016-09-28 22:41:54 -0400

    FEATURE: Player Creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925100239) do
+ActiveRecord::Schema.define(version: 20160925101836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "characters", force: :cascade do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "stages", force: :cascade do |t|
     t.string   "name"
     t.string   "nickname"
     t.text     "description"
